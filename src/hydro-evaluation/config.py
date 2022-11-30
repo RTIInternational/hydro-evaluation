aedc0bdc8d3d802c582eb93abb383702fff1f330
Matt Denno <mgdenno@gmail.com>
2022-11-29 20:43:27 -0500

    wip - start adding df process

--- src/hydro-evaluation/config.py
+++ src/hydro-evaluation/config.py
@@ -1,3 +1,13 @@
+
+# PostgreSQL database connection string
 CONNECTION = "postgresql://postgres:postgrespassword@localhost:5432/postgres"
+
 STRING_TAG_TYPES = ["nwm_feature_id", "usgs_site_code", "configuration", "measurement_unit", "variable_name"]
+
 DATETIME_TAG_TYPES = ["reference_time"]
+
+FLOAT_TAG_TYPES = []
+
+INT_TAG_TYPES = []
+
+GEO_TAG_TYPES = []
--- src/hydro-evaluation/load/test_hydrotools.py
+++ src/hydro-evaluation/load/test_hydrotools.py
@@ -1,14 +1,19 @@
 # Import the nwm Client
-from typing import List
+from typing import List, Any
 
 import config
+import utils
+import time
 import pandas as pd
 # Import the NWIS IV Client
 from hydrotools.nwis_client.iv import IVDataService
 from hydrotools.nwm_client import gcp as nwm
-from models import DateTimeTagType, StringTagType
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, insert
 from sqlalchemy.orm import Session
+from models import StringTagTypes, DateTimeTagTypes, StringTags, DateTimeTags, Timeseries
+
+
+
 
 engine = create_engine(config.CONNECTION, echo=True, future=True)
 
@@ -27,12 +32,12 @@ def fetch_nwm():
     #  CONUS that are used for model assimilation
     forecast_data = model_data_service.get(
         configuration = "medium_range_mem1",
-        reference_time = "20221103T00Z"
+        reference_time = "20221104T00Z"
         )
 
     # Look at the data
-    print(forecast_data.info(memory_usage='deep'))
-    print(forecast_data)
+    # print(forecast_data.info(memory_usage='deep'))
+    # print(forecast_data)
     return forecast_data
 
     
@@ -56,50 +61,157 @@ def df_to_evaldb(
     unq_cols: List[str],
     tag_cols: List[str],
 ):
-    """Write pd.DataFrame to eval database.
-    """
-    # get tags that are the same for all timeseries
-    get_tag_types(tag_cols)
+    """Write pd.DataFrame to eval database."""
 
-    get_unique_tag_values(df, tag_cols)
+    # start = time.perf_counter()
+    # time.sleep(1) # do work
+    # elapsed = time.perf_counter() - start
+    # print(f'Time {elapsed:0.4}')
 
-def get_tag_types(tag_names: List[str]):
-    with Session(engine) as session:
-        stmt = select(StringTagType).where(StringTagType.name.in_(tag_names))
+    df = df[df['nwm_feature_id'] == 1180000535]
 
-        for user in session.scalars(stmt):
-            print(user)
+    df_tags = create_df_tags(df, tag_cols)
+
+    groups = df.groupby(unq_cols)
+    print(len(groups))
+
+    for name, group in groups:
+        print(name)
+        print(group)
+        ts_tags = create_ts_tags(unq_cols, name)
+        with Session(engine) as session:
+            ts = Timeseries(name="_".join([str(v) for v in name]))
+            session.add(ts)
+            session.commit()
+
+
+# def get_tag_types(tag_names: List[str]):
+#     with Session(engine) as session:
+#         stmt = select(StringTagTypes).where(StringTagTypes.name.in_(tag_names))
+
+#         for row in session.scalars(stmt):
+#             print(row)
     
-    with Session(engine) as session:
-        stmt = select(DateTimeTagType).where(DateTimeTagType.name.in_(tag_names))
+#     with Session(engine) as session:
+#         stmt = select(DateTimeTagTypes).where(DateTimeTagTypes.name.in_(tag_names))
 
-        for user in session.scalars(stmt):
-            print(user)
+#         for row in session.scalars(stmt):
+#             print(row)
 
 
-def get_unique_tag_values(df: pd.DataFrame, tag_names: List[str]):
-    for tag_name in tag_names:
-        if tag_name in config.STRING_TAG_TYPES:
-            unique_values = df[tag_name].unique()
+def create_df_tags(df: pd.DataFrame, tag_type_names: List[str]) -> List:
+    """Get tags that will be applied to every timeseries in DataFrame.
+
+    Looks in each column provided in tag_type_names and creates tags in database.
+
+    ToDo: this pattern might not be too good, will work for now.
+    
+
+        Parameters
+        ----------
+            df: 
+            tag_type_names:
+
+        Returns
+        -------
+            tags:
+    """
+    tags = []
+    
+    for tag_type_name in tag_type_names:
+        if tag_type_name in config.STRING_TAG_TYPES:
+            unique_values = df[tag_type_name].unique()
             if len(unique_values) > 1:
-                raise ValueError(f"There is more than 1 unique {tag_name} value in DataFrame.")
+                raise ValueError(f"There is more than 1 unique {tag_type_name} value in DataFrame.")
             if len(unique_values) == 0:
-                raise ValueError(f"Tag name {tag_name} does not exist in DataFrame.")
-            print(f"Found {tag_name} {unique_values[0]} ")
+                raise ValueError(f"Tag name {tag_type_name} does not exist in DataFrame.")
+            print(f"Found {tag_type_name} {unique_values[0]} ")
 
+            with Session(engine) as session:
+                tag, cr = utils.get_or_create(
+                    session, 
+                    StringTags,
+                    value=str(unique_values[0]),
+                    string_tag_type_name = tag_type_name
+                )
+                tags.append({
+                    "tag_type": "string_tag",
+                    "id": tag.id
+                })
 
-        if tag_name in config.DATETIME_TAG_TYPES:
-            unique_values = df[tag_name].unique()
+        if tag_type_name in config.DATETIME_TAG_TYPES:
+            unique_values = df[tag_type_name].unique()
             if len(unique_values) > 1:
-                raise ValueError(f"There is more than 1 unique {tag_name} value in DataFrame.")
+                raise ValueError(f"There is more than 1 unique {tag_type_name} value in DataFrame.")
             if len(unique_values) == 0:
-                raise ValueError(f"Tag name {tag_name} does not exist in DataFrame.")
-            print(f"Found {tag_name} {unique_values[0]} ")
+                raise ValueError(f"Tag name {tag_type_name} does not exist in DataFrame.")
+            print(f"Found {tag_type_name} {unique_values[0]} ")
+
+            with Session(engine) as session:
+                tag, cr = utils.get_or_create(
+                    session, 
+                    DateTimeTags,
+                    value=pd.Timestamp(unique_values[0]),
+                    datetime_tag_type_name = tag_type_name
+                )
+                tags.append({
+                    "tag_type": "datetime_tag",
+                    "id": tag.id
+                })
+
+    return tags
 
 
+def create_ts_tags(tag_type_names: List[str], tag_values: List[Any]) -> List:
+    """Create tags for a single timeseries
 
+    ToDo: this pattern might not be too good, will work for now.
+    
 
+        Parameters
+        ----------
+            df: 
+            tag_type_names:
 
+        Returns
+        -------
+            tags:
+    """
+    if len(tag_type_names) != len(tag_values):
+        raise ValueError(f"Length of tag_type_names must equal length of tag_values")
+    
+    tags = []
+
+    for i, tag_type_name in enumerate(tag_type_names):
+        if tag_type_name in config.STRING_TAG_TYPES:
+
+            with Session(engine) as session:
+                tag, cr = utils.get_or_create(
+                    session, 
+                    StringTags,
+                    value=str(tag_values[i]),
+                    string_tag_type_name = tag_type_name
+                )
+                tags.append({
+                    "tag_type": "string_tag",
+                    "id": tag.id
+                })
+
+        if tag_type_name in config.DATETIME_TAG_TYPES:
+
+            with Session(engine) as session:
+                tag, cr = utils.get_or_create(
+                    session, 
+                    DateTimeTags,
+                    value=pd.Timestamp(tag_values[i]),
+                    datetime_tag_type_name = tag_type_name
+                )
+                tags.append({
+                    "tag_type": "datetime_tag",
+                    "id": tag.id
+                })
+
+    return tags
 
 
 if __name__ == "__main__":
