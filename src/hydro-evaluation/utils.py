f2c3e74535a3478eebafec7c11bba976c373df84
Matt Denno <mgdenno@gmail.com>
2022-12-05 21:05:08 -0500

    ingest and parse basically working...

--- src/hydro-evaluation/utils.py
+++ src/hydro-evaluation/utils.py
@@ -3,9 +3,21 @@ from io import StringIO
 from typing import List
 
 import pandas as pd
+import numpy as np
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session, declarative_base
+from psycopg2.extensions import register_adapter, AsIs
 
+def addapt_numpy_float64(numpy_float64):
+    return AsIs(numpy_float64)
+def addapt_numpy_int64(numpy_int64):
+    return AsIs(numpy_int64)
+def addapt_numpy_datetime64(numpy_datetime64):
+    return AsIs(numpy_datetime64)
+
+register_adapter(np.float64, addapt_numpy_float64)
+register_adapter(np.int64, addapt_numpy_int64)
+register_adapter(np.datetime64, addapt_numpy_datetime64)
 
 def get_or_create(session, model, defaults=None, **kwargs):
     instance = session.query(model).filter_by(**kwargs).one_or_none()
@@ -137,6 +149,45 @@ def insert_bulk(session: Session, df: pd.DataFrame, table_name: str, columns: Li
         # but tables sizes where growing without any new data modifications
         # if this command isn't executed)
         cursor.execute(f"DROP TABLE {temp_table_name}")
+
+        # Commit everything through cursor
+        conn.commit()
+
+    conn.close()
+
+    return records
+
+
+def insert_multi(session: Session, df: pd.DataFrame, table_name: str, columns: List[str], returning: List[str] = ["id"]):
+    """
+
+    """
+
+    # tups = df.itertuples(index=False, name=None)
+    data = [tuple(r) for r in df.to_numpy()]
+    
+    conn = session.connection().connection
+    with conn.cursor() as cursor:
+        args_str = ','.join(cursor.mogrify(f"({','.join(['%s' for c in columns])})", x).decode('utf-8') for x in data)
+
+        # Inserts copied data from the temporary table to the final table
+        # updating existing values at each new conflict
+        if len(returning) > 0:
+            cursor.execute(
+                f"""
+                INSERT INTO {table_name}({', '.join(columns)})
+                VALUES {args_str} ON CONFLICT DO NOTHING RETURNING {', '.join(returning)};
+                """
+            )
+            records = cursor.fetchall()
+        else:
+            cursor.execute(
+                f"""
+                INSERT INTO {table_name}({', '.join(columns)})
+                VALUES {args_str} ON CONFLICT DO NOTHING;
+                """
+            )
+            records = None
 
         # Commit everything through cursor
         conn.commit()
