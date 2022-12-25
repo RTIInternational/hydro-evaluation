bffdec67fea7b994277a44944d3d542e84983108
Matt Denno <mgdenno@gmail.com>
2022-12-25 11:15:19 -0500

    wip raster stuff

---
+++ src/hydro-evaluation/grids/config.py
@@ -0,0 +1,6 @@
+import os
+# import tempfile
+
+CONNECTION = "postgresql://postgres:postgrespassword@localhost:5432/postgres"
+# TEMP_DIR = tempfile.gettempdir()
+NWM_CACHE = os.path.join("/home", "matt", "cache")
