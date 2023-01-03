0b4d0653521ed6386f6be03cac99ee8305f06d5f
Matt Denno <mgdenno@gmail.com>
2023-01-02 19:36:06 -0500

    play with MVT on the fly

--- src/hydro-evaluation/grids/query_map.py
+++ src/hydro-evaluation/grids/query_map.py
@@ -18,12 +18,78 @@ import grids.config as config
 from grids.utils import profile
 
 
-def format_filters(filters):
-        return [f"""{f["column"]} {f["operator"]} '{f["value"]}'""" for f in filters]
+def format_filters(filters: List[dict]) -> List[str]:
+    """Generate strings from filter dict.
+    
+   ToDo:  This is probably not robust enough.
+    """
+    filter_strs = []
+    for f in filters:
+        if type(f["value"]) == str:
+            filter_strs.append(f"""{f["column"]} {f["operator"]} '{f["value"]}'""")
+        else:
+            filter_strs.append(f"""{f["column"]} {f["operator"]} {f["value"]}""")
+    return filter_strs
 
 
 def generate_map_query(group_by: List[str], order_by: List[str], filters: List[dict]) -> str:
-    """Generate query string for getting MAP"""
+    """Generate query string for getting MAP.
+
+    ToDo: This needs to check for valid column names.
+    
+    Parameters
+    ----------
+    group_by: List[str]
+        List of field names to select and group by.  
+            This will likely always include the unique polygon name (e.g. huc10)
+    order_by: List[str]
+        List of fields to order results by.
+    filters: List[dict]
+        List of dictionaries containing `column`, `operator` and `value` that will be used 
+            in the where clause of the query to filter results.  Columns referenced in filters 
+            must be in `group_by`.
+
+    Returns
+    -------
+    query: str
+        Query to get MAP from rasters in database 
+    
+    Examples
+    --------
+
+    group_by=[
+                "huc10",
+                "reference_time", 
+                "geom",
+                "value_time",
+                "lead_time",
+            ],
+    order_by=["reference_time", "huc10"],
+    filters=[
+        {
+            "column": "huc10",
+            "operator": "like",
+            "value": "0303%"
+        },
+        {
+            "column": "reference_time",
+            "operator": "=",
+            "value": "2022-10-01 00:00:00"
+        },
+        {
+            "column": "lead_time",
+            "operator": "=",
+            "value": 1
+        },
+        {
+            "column": "lead_time",
+            "operator": "in",
+            "value": "(6, 12, 18, 24)"
+        }
+    ]
+
+
+    """
 
     query = f"""
         WITH medium_range_map AS (
@@ -45,48 +111,92 @@ def generate_map_query(group_by: List[str], order_by: List[str], filters: List[d
 
 
 @profile
-def get_map():
-    """Get MAP from database."""  
-
-    for i in range(5):
-
-        query = generate_map_query(
-            group_by=[
-                "huc10",
-                "reference_time", 
-                "geom",
-                # "value_time",
-                "lead_time",
-            ],
-            order_by=["reference_time", "huc10"],
-            filters=[
-                {
-                    "column": "huc10",
-                    "operator": "like",
-                    "value": "0303%"
-                },
-                {
-                    "column": "reference_time",
-                    "operator": "=",
-                    "value": "2022-10-01 00:00:00"
-                },
-                {
-                    "column": "lead_time",
-                    "operator": "=",
-                    "value": str(i + 1)
-                }
-                
-            ]
-        )
+def get_map(group_by: List[str], order_by: List[str], filters: List[dict], return_geo: bool = True) -> gpd.GeoDataFrame:
+    """Get MAP from database as a gpd.GeoDataFrame.
     
-        # print(query)
-        df = pd.read_sql(text(query), config.CONNECTION)
+    See `generate_map_query` for parameter descriptions.
+    """  
 
+    query = generate_map_query(
+        group_by=group_by,
+        order_by=order_by,
+        filters=filters
+    )
+
+    # Get DataFrame from database
+    df = pd.read_sql(text(query), config.CONNECTION)
+
+    if return_geo:
+        # Convert to GeoDataFrame
         gs = gpd.GeoSeries.from_wkb(df["geom"])
         gdf = gpd.GeoDataFrame(df, geometry=gs)
-        gdf.plot("value", legend=True)
-        plt.savefig(f"2022-10-01_00:00:00_t{i + 1}.png")
+
+        return gdf
+
+    return df
     
+def main():
+
+    # for i in range(5):
+    #     gdf = get_map(
+    #         group_by=[
+    #             "huc10",
+    #             "reference_time", 
+    #             "geom",
+    #             # "value_time",
+    #             "lead_time",
+    #         ],
+    #         order_by=["reference_time", "huc10"],
+    #         filters=[
+    #             {
+    #                 "column": "huc10",
+    #                 "operator": "like",
+    #                 "value": "0303%"
+    #             },
+    #             {
+    #                 "column": "reference_time",
+    #                 "operator": "=",
+    #                 "value": "2022-10-01 00:00:00"
+    #             },
+    #             {
+    #                 "column": "lead_time",
+    #                 "operator": "=",
+    #                 "value": str(i + 1)
+    #             }
+    #         ])
+    #     gdf.plot("value", legend=True)
+    #     plt.savefig(f"2022-10-01_00:00:00_t{i + 1}.png")
+
+    
+    df = get_map(
+        group_by=[
+            "huc10",
+            "reference_time", 
+            "value_time"
+        ],
+        order_by=["reference_time", "huc10"],
+        filters=[
+            {
+                "column": "huc10",
+                "operator": "like",
+                "value": "0303%"
+            },
+            {
+                "column": "reference_time",
+                "operator": "=",
+                "value": "2022-10-01 00:00:00"
+            }
+        ],
+    return_geo=False
+    )
+    print(df)
+
+    for k,v in df.groupby("huc10"):
+        plt.plot(v["value_time"], v["value"])
+        
+    plt.savefig(f"2022-10-01_00:00:00.png")
+
+
 
 if __name__ == "__main__":
-    get_map()
+    main()
--- src/hydro-evaluation/wide_table/scratch.sql
+++ src/hydro-evaluation/wide_table/scratch.sql
@@ -85,3 +85,73 @@ END
 $$;
 
 SELECT * FROM get_upstream(9630389);
+
+-- Try tiling query
+WITH 
+    bounds AS (
+        SELECT ST_Segmentize(ST_MakeEnvelope(-8296780.798186153, 4970241.32721529, -8257645.039704142, 5009377.085697301, 3857),9783.939620502759) AS geom, 
+               ST_Segmentize(ST_MakeEnvelope(-8296780.798186153, 4970241.32721529, -8257645.039704142, 5009377.085697301, 3857),9783.939620502759)::box2d AS b2d
+    ),
+    mvtgeom AS (
+        SELECT ST_AsMVTGeom(ST_Transform(t.geom, '+proj=lcc +lat_0=40 +lon_0=-97 +lat_1=30 +lat_2=60 +x_0=0 +y_0=0 +R=6370000 +units=m +no_defs +type=crs'::TEXT, 3857), bounds.b2d) AS geom, 
+               gid, huc10
+        FROM huc10 t, bounds
+        WHERE ST_Intersects(t.geom, ST_Transform(bounds.geom, 
+        	'+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs'::text, 
+			'+proj=lcc +lat_0=40 +lon_0=-97 +lat_1=30 +lat_2=60 +x_0=0 +y_0=0 +R=6370000 +units=m +no_defs +type=crs'::TEXT)
+			)
+	    ) 
+    SELECT ST_AsMVT(mvtgeom.*) FROM mvtgeom;
+
+   
+SELECT ST_SRID(geom) FROM huc10_mean LIMIT 1;
+   
+   
+-- create a huc map view
+DROP VIEW huc10_mean;
+CREATE VIEW huc10_mean AS (
+	WITH medium_range_map AS (
+	    SELECT
+	        gid,huc10,reference_time,geom,lead_time,
+	        (ST_SummaryStatsAgg(ST_Clip(rast, geom, true), 1, true)).mean as value
+	    FROM
+	        forcing_medium_range_attrs as raster
+	    INNER JOIN huc10 on
+	        ST_INTERSECTS(geom, rast)
+	    GROUP BY
+	        huc10,reference_time,geom,lead_time,gid
+	    ORDER BY
+	        reference_time,huc10
+	)
+	SELECT * FROM medium_range_map WHERE reference_time = '2022-10-01 00:00:00' AND lead_time = '1'
+);
+
+SELECT * FROM huc10_mean WHERE huc10 LIKE '0303%';
+
+SELECT count(*) FROM huc10;
+
+
+WITH 
+    bounds AS (
+        SELECT ST_Segmentize(ST_MakeEnvelope(-8218509.281222133, 5009377.085697301, -8140237.764258113, 5087648.602661321, 3857),19567.879241005052) AS geom, 
+               ST_Segmentize(ST_MakeEnvelope(-8218509.281222133, 5009377.085697301, -8140237.764258113, 5087648.602661321, 3857),19567.879241005052)::box2d AS b2d
+    ),
+    mvtgeom AS (
+        SELECT ST_AsMVTGeom(ST_Transform(
+                t.geom, 
+                '+proj=lcc +lat_0=40 +lon_0=-97 +lat_1=30 +lat_2=60 +x_0=0 +y_0=0 +R=6370000 +units=m +no_defs +type=crs'::TEXT, 
+                3857
+            ), 
+        bounds.b2d) AS geom, 
+               gid, huc10, value
+        FROM huc10_mean t, bounds
+        WHERE ST_Intersects(t.geom, ST_Transform(
+            bounds.geom,
+            '+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs'::text, 
+                        '+proj=lcc +lat_0=40 +lon_0=-97 +lat_1=30 +lat_2=60 +x_0=0 +y_0=0 +R=6370000 +units=m +no_defs +type=crs'::TEXT
+            )
+        )
+    ) 
+    SELECT ST_AsMVT(mvtgeom.*) FROM mvtgeom
+            
+            
