165cd3d4eafd89cf6abb2610f8d8a12e4077c968
Matt Denno <mgdenno@gmail.com>
2023-01-23 16:44:58 -0500

    reorg and do some clean up

---
+++ src/evaluation/loading/basin_to_parquet.py
@@ -0,0 +1,11 @@
+import config
+from utils import shape_to_gdf, gdf_to_parquet
+
+
+def main():
+    """Convert HUC10 shapefile to parquet."""
+    huc10_gdf = shape_to_gdf(config.HUC10_SHP_FILEPATH)
+    gdf_to_parquet(huc10_gdf, config.HUC10_PARQUET_FILEPATH)
+
+if __name__ == "__main__":
+    main()
