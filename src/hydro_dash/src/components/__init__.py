b1b51fa5fb0cc3470cafd7c35d9e2f0f5f99ebfa
Chris Townsend <ctownsend@rti.org>
2023-02-28 12:11:59 -0500

    Removes Dash based project structure.

--- src/hydro_dash/src/components/__init__.py
+++
@@ -1,5 +0,0 @@
-# pull in components from files in the current directory to make imports cleaner
-from .navbar import navbar
-from .footer import footer
-from .dog_image import create_dog_image_card
-from .number_fact_aio import NumberFactAIO
--- src/hydro_dash/src/pages/complex_page/comp1.py
+++
@@ -1,9 +0,0 @@
-#notes
-'''
-If a page has a lot of components, I suggest splitting them up into their own files such as this one.
-'''
-
-# package imports
-from dash import html
-
-random_component = html.Div('This is a random component specific to the current page.')
